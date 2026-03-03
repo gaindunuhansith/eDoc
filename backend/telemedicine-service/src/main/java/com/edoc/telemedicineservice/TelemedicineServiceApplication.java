5498914d6a57424abe5f4a4e17b40b2342ca295d
Gaindu Nuhansith <136603140+gaindunuhansith@users.noreply.github.com>
2026-03-04 00:26:29 +0530

    feat: initial commit

---
+++ backend/telemedicine-service/src/main/java/com/edoc/telemedicineservice/TelemedicineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.edoc.telemedicineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemedicineServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelemedicineServiceApplication.class, args);
+    }
+
+}
