d67feb0a817ce6eb33884c8b0e1a972ee26d4185
Gaindu Nuhansith <136603140+gaindunuhansith@users.noreply.github.com>
2026-04-13 23:05:09 +0530

    feat: add Payment and PaymentLog repository interfaces

---
+++ backend/payment-service/src/main/java/com/edoc/paymentservice/repository/PaymentLogRepository.java
@@ -0,0 +1,10 @@
+package com.edoc.paymentservice.repository;
+
+import com.edoc.paymentservice.entity.PaymentLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentLogRepository extends JpaRepository<PaymentLog, Long> {
+    
+}
