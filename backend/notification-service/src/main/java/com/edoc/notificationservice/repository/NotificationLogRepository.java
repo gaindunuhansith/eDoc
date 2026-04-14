0e57d6001a68afa815bf9674ebbdc9b30535b495
LehanNawaratne <162142452+LehanNawaratne@users.noreply.github.com>
2026-04-14 16:14:01 +0530

    remove notification service

--- backend/notification-service/src/main/java/com/edoc/notificationservice/repository/NotificationLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.edoc.notificationservice.repository;
-
-import com.edoc.notificationservice.entity.NotificationLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationLogRepository extends JpaRepository<NotificationLog, Long> {
-}
