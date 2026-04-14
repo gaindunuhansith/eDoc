08489209d4e6385e380b29c627e2263e18780517
Gaindu Nuhansith <136603140+gaindunuhansith@users.noreply.github.com>
2026-04-14 23:41:43 +0530

    feat: add custom exception classes for user service

---
+++ backend/user-service/src/main/java/com/edoc/userservice/exception/UnauthorizedOperationException.java
@@ -0,0 +1,8 @@
+package com.edoc.userservice.exception;
+
+public class UnauthorizedOperationException extends RuntimeException {
+
+    public UnauthorizedOperationException(String message) {
+        super(message);
+    }
+}
