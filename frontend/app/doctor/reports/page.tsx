a56372dc5c9588893dc7ea26cbf7a1dd52eb3ac0
Gaindu Nuhansith <136603140+gaindunuhansith@users.noreply.github.com>
2026-04-16 19:16:40 +0530

    feat: add basic structure for admin and patient pages with layout components

---
+++ frontend/app/doctor/reports/page.tsx
@@ -0,0 +1,9 @@
+export default function ReportsPage() {
+  return (
+    <div className="bg-white p-6 rounded-xl border border-gray-100 min-h-125">
+      {" "}
+      <h2 className="text-2xl font-semibold mb-4 capitalize">Reports</h2>{" "}
+      <p className="text-gray-500">This is the doctor/reports page.</p>{" "}
+    </div>
+  );
+}
---
+++ frontend/app/patient/appointments/page.tsx
@@ -0,0 +1,13 @@
+export default function AppointmentsPage() {
+  return (
+    <div className="bg-white p-6 rounded-xl border border-gray-100 min-h-125">
+      {" "}
+      <h2 className="text-2xl font-semibold mb-4 capitalize">
+        Appointments
+      </h2>{" "}
+      <p className="text-gray-500">
+        This is the patient/appointments page.
+      </p>{" "}
+    </div>
+  );
+}
